4e09f115bba81f77d9b20ac94eb7c92c86f5baf3
JoJo <josephhe255@gmail.com>
2024-08-05 20:34:08 +0800

    add edit note and popup

--- frontend/notes-app/src/components/SearchBar/SearchBar.jsx
+++ frontend/notes-app/src/components/SearchBar/SearchBar.jsx
@@ -1,20 +1,20 @@
 import React from 'react'
-import {FaMagnifyingGlass} from "react-icons/fa6";
-import {IoMdClose} from "react-icons/io";
+import { FaMagnifyingGlass } from "react-icons/fa6";
+import { IoMdClose } from "react-icons/io";
 
-const SearchBar = ({value, onChange, handleSearch, onClearSearch}) => {
+const SearchBar = ({ value, onChange, handleSearch, onClearSearch }) => {
   return (
     <div className="w-80 flex items-center px-4 bg-slate-100 rounded-md">
-      <input type="text" 
+      <input type="text"
         placeholder='Search Notes'
         className="w-full text-xs bg-transparent py-[11px] outline-none"
         value={value}
         onChange={onChange}
       />
 
-      { value && (
-        <IoMdClose 
-          className="text-xl text-slate-500 cursor-pointer hover:text-black" 
+      {value && (
+        <IoMdClose
+          className="text-xl text-slate-500 cursor-pointer hover:text-black"
           onClick={onClearSearch}
         />
       )}
