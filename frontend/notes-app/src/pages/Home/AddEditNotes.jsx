779ce1d49a43867fb75e42f4905e982928a8d1c0
JoJo <josephhe255@gmail.com>
2024-08-05 20:46:25 +0800

    handle error before adding note

--- frontend/notes-app/src/pages/Home/AddEditNotes.jsx
+++ frontend/notes-app/src/pages/Home/AddEditNotes.jsx
@@ -1,14 +1,39 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import TagInput from '../../components/Input/TagInput'
 import { MdClose } from "react-icons/md";
 
-const AddEditNotes = ({onClose}) => {
+const AddEditNotes = ({ noteData, type, onClose }) => {
 
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
   const [tags, setTags] = useState([]);
 
+  const [error, setError] = useState(null);
 
+
+  const addNewNote = async () => { }
+  const editNote = async () => { }
+
+
+  const handleAddNote = () => {
+    if (!title) {
+      setError("Please enter the title");
+      return;
+    }
+
+    if (!content) {
+      setError("Please enter the content");
+      return;
+    }
+
+    setError("");
+
+    if (type === "edit") {
+      editNote();
+    } else {
+      addNewNote();
+    }
+  };
   return (
     <div className="relative">
       <button className="w-10 h-10 rouded-full flex items-center justify-center absolute -top-3 -right-3 hover:bg-slate-50"
@@ -23,7 +48,7 @@ const AddEditNotes = ({onClose}) => {
           className="text-2xl text-slate-950 outline-none"
           placeholder="Go To Gym At 5"
           value={title}
-          onChange={(target) => {setTitle(target.value)}}
+          onChange={(target) => { setTitle(target.value) }}
         />
       </div>
       <div className="flex flex-col gap-2 mt-4">
@@ -36,16 +61,20 @@ const AddEditNotes = ({onClose}) => {
           placeholder="Content"
           rows={10}
           value={content}
-          onChange={(target) => {setContent(target.value)}}
+          onChange={(target) => { setContent(target.value) }}
         />
       </div>
 
       <div className="mt-3">
         <label className="input-label">TAGS</label>
-        <TagInput tags={tags} setTags={setTags}/>
+        <TagInput tags={tags} setTags={setTags} />
       </div>
 
-      <button className="btn-primary font-medium mt-5 p-3" onClick={() => { }}>
+      {error &&
+        <p className="text-red-500 text-xs pt-4">{error}</p>
+      }
+      <button className="btn-primary font-medium mt-5 p-3"
+        onClick={handleAddNote}>
         ADD
       </button>
     </div>
