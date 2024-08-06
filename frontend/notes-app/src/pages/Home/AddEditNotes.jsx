95d6fd2dfbaf0738fbd315b020b4d4a1a58a7fd7
JoJo <josephhe255@gmail.com>
2024-08-06 22:02:42 +0800

    pop up toast message after add/edit notes

--- frontend/notes-app/src/pages/Home/AddEditNotes.jsx
+++ frontend/notes-app/src/pages/Home/AddEditNotes.jsx
@@ -3,7 +3,7 @@ import TagInput from "../../components/Input/TagInput";
 import { MdClose } from "react-icons/md";
 import axiosInstance from "../../utils/axiosInstance";
 
-const AddEditNotes = ({ noteData, type, getAllNotes, onClose }) => {
+const AddEditNotes = ({ noteData, type, getAllNotes, onClose, showToastMessage }) => {
   const [title, setTitle] = useState(noteData?.title || "");
   const [content, setContent] = useState(noteData?.content || "");
   const [tags, setTags] = useState([]);
@@ -18,6 +18,7 @@ const AddEditNotes = ({ noteData, type, getAllNotes, onClose }) => {
         tags,
       });
       if (response.data && response.data.note) {
+        showToastMessage("Note Added Successfully");
         getAllNotes();
         onClose();
       }
@@ -41,6 +42,7 @@ const AddEditNotes = ({ noteData, type, getAllNotes, onClose }) => {
         tags,
       });
       if (response.data && response.data.note) {
+        showToastMessage("Note Updated Successfully");
         getAllNotes();
         onClose();
       }
