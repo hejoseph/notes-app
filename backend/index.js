59792e7ac5f7582575b6ecc528bb6fa6e55fe4bd
JoJo <josephhe255@gmail.com>
2024-08-06 14:23:45 +0800

    get-user api when logged

--- backend/index.js
+++ backend/index.js
@@ -116,6 +116,25 @@ app.post("/login", async (req, res) => {
 
 });
 
+
+app.get("/get-user", authenticateToken, async (req, res) => {
+  const { user } = req.user;
+  const isUser = await User.findOne({ _id: user._id });
+  if(!isUser){
+    return res.sendStatus(401);
+  }
+
+  return res.json({
+    user: {
+      fullName: isUser.fullName,
+      email: isUser.email,
+      _id: isUser._id,
+      createdOn: isUser.createOn
+    },
+    message: ""
+  });
+});
+
 app.post("/add-note", authenticateToken, async (req, res) => {
   const { title, content, tags } = req.body;
   const { user } = req.user;
